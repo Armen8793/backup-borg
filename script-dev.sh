2d9e558148023c59761b12ad68c9aef17d31bdd4
Armen8793 <80597213+Armen8793@users.noreply.github.com>
2023-09-25 10:23:58 +0400

    Update script-dev.sh

--- script-dev.sh
+++ script-dev.sh
@@ -9,6 +9,12 @@ SSH_KEY="ssh key full path"
 SOURCE_DIRS="directories which you want to backup"
 # Archive name with timestamp
 ARCHIVE_NAME="$(hostname)-$(date +'%Y-%m-%d_%H-%M')"
+current_hostname=$(hostname)
+VOLUMES="/path/to/pg_volumes.txt"
+PGDUMP_DIR="/root/postgres-dump"
+PSQL_USER="postgres user"
+PSQL_PWD="postgres password"
+PGB_PORT="6432"
 
 # Set the environment variable
 export BORG_UNKNOWN_UNENCRYPTED_REPO_ACCESS_IS_OK=1
@@ -21,12 +27,22 @@ else
     echo "Repository alredy exists"
 fi
 
+
+if [[ $current_hostname == *"postgresql"* ]]; then
+    echo "Hostname contains 'postgresql'."
+    PGPASS_FILE="/root/.pgpass"
+    echo "localhost:$PGB_PORT:*:$PSQL_USER:$PSQL_PWD" > "$PGPASS_FILE"
+    chmod 600 "$PGPASS_FILE"
+    pg_dump -h localhost -p $PGB_PORT -U $PSQL_USER > backup1.sql
+else
+   echo "Hostname does not contain 'postgresql'."
+fi
 #borg init -e none $BACKUP_USER@$BACKUP_SERVER:$BORG_REPO
 
-
-for dir in "${SOURCE_DIRS[@]}"; do
+#for dir in "${SOURCE_DIRS}"
+for dir in "$(cat ~/borgbackup-dev/backup-dirs.txt)"; do
     echo "Backing up $dir..."
-    borg create -s --list --progress $BACKUP_USER@$BACKUP_SERVER:$BORG_REPO::$ARCHIVE_NAME $SOURCE_DIRS $dir
+    borg create -s --progress $BACKUP_USER@$BACKUP_SERVER:$BORG_REPO::$ARCHIVE_NAME $dir
     if [ $? -eq 0 ]; then
         echo "Backup of $dir completed successfully."
     else
@@ -34,9 +50,34 @@ for dir in "${SOURCE_DIRS[@]}"; do
     fi
 done
 
+if [[ $current_hostname == *"docker"* ]]; then
+    echo "Hostname contains 'docker'."
+    logs_directory="/var/lib/docker/volumes"
+    find "$logs_directory" -type d -name "*logs*" -exec find {} -type f \; | while read -r log_file; do
+        log_filename=$(basename "$log_file")
+        scp -i "$SSH_KEY" "$log_file" "$BACKUP_USER@$BACKUP_SERVER:$BORG_REPO/$current_hostname-volumes-backup"
+    done
+
+    find "$logs_directory" -type d -name "*exch*" -exec find {} -type f \; | while read -r log_file; do
+        log_filename=$(basename "$log_file")
+        scp -i "$SSH_KEY" "$log_file" "$BACKUP_USER@$BACKUP_SERVER:$BORG_REPO/$current_hostname-volumes-backup"
+    done
+
+    find "$logs_directory" -type d -name "*data*" -exec find {} -type f \; | while read -r log_file; do
+        log_filename=$(basename "$log_file")
+        scp -i "$SSH_KEY" "$log_file" "$BACKUP_USER@$BACKUP_SERVER:$BORG_REPO/$current_hostname-volumes-backup"
+    done
+
+    find "$logs_directory" -type d -name "*monitoring*" -exec find {} -type f \; | while read -r log_file; do
+        log_filename=$(basename "$log_file")
+        scp -i "$SSH_KEY" "$log_file" "$BACKUP_USER@$BACKUP_SERVER:$BORG_REPO/$current_hostname-volumes-backup"
+    done
+fi
+
 
 # Prune old backups on the local repository
 borg prune -v $BACKUP_USER@$BACKUP_SERVER:$BORG_REPO --keep-daily=7 --keep-weekly=4 --keep-monthly=1
+
 
 
 
