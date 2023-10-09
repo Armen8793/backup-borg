8e287354838e9d64297c606d515973e2dcf9cc33
Armen8793 <80597213+Armen8793@users.noreply.github.com>
2023-10-09 11:44:37 +0400

    Update script-dev.sh

--- script-dev.sh
+++ script-dev.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Set variables
-BORG_REPO="your backup server borg repo here/$(hostname)"
-BACKUP_SERVER="backup server dns or ip"
-BACKUP_USER="backup username"
-LOCAL_REPO="local borg repo full path"
-SSH_KEY="ssh key full path"
-SOURCE_DIRS="directories which you want to backup"
+BORG_REPO="/home/ansible-abb/backup-test/$(hostname)"
+BACKUP_SERVER="192.168.18.53"
+BACKUP_USER="ansible-abb"
+#LOCAL_REPO="/home/armen/backup"
+SSH_KEY="~/.ssh/ansible-abb"
+#SOURCE_DIRS="/srv /opt"
 # Archive name with timestamp
 ARCHIVE_NAME="$(hostname)-$(date +'%Y-%m-%d_%H-%M')"
 current_hostname=$(hostname)
-VOLUMES="/path/to/pg_volumes.txt"
-PGDUMP_DIR="/root/postgres-dump"
-PSQL_USER="postgres user"
-PSQL_PWD="postgres password"
+#VOLUMES="/home/ansible-abb/backup-test/pg_volumes.txt"
+#PGDUMP_DIR="/root/postgres-dump"
+PSQL_USER="postgres"
+PSQL_PWD="8FbWhlyMJCjxotGgew2l"
 PGB_PORT="6432"
 
 # Set the environment variable
@@ -27,45 +27,44 @@ else
     echo "Repository alredy exists"
 fi
 
-
 if [[ $current_hostname == *"postgresql"* ]]; then
     echo "Hostname contains 'postgresql'."
-    PGPASS_FILE="/root/.pgpass"
+    PGPASS_FILE="/home/ansible-abb/.pgpass"
+    sudo chown -R ansible-abb:ansible-abb "$PGPASS_FILE"
     echo "localhost:$PGB_PORT:*:$PSQL_USER:$PSQL_PWD" > "$PGPASS_FILE"
     chmod 600 "$PGPASS_FILE"
-    pg_dump -h localhost -p $PGB_PORT -U $PSQL_USER > backup1.sql
+    pg_dump -h localhost -p $PGB_PORT -U $PSQL_USER | gzip > ~/borgbackup-dev/${ARCHIVE_NAME}.dump.gz
 else
    echo "Hostname does not contain 'postgresql'."
 fi
-#borg init -e none $BACKUP_USER@$BACKUP_SERVER:$BORG_REPO
 
-#for dir in "${SOURCE_DIRS}"
 
 if [[ $current_hostname == *"docker"* ]]; then
-    for volume in $(docker volume ls --filter "name=logs" --format={{.Name}}); do
-        VOLUME_BACKUP=$(docker volume ls --filter "name=${volume}" --format={{.Name}})
-        echo ${VOLUME_BACKUP} >> backup-dirs.txt
+    for volume in $(sudo docker volume ls --filter "name=logs" --format={{.Name}}); do
+        VOLUME_BACKUP=$(sudo docker volume ls --filter "name=${volume}" --format={{.Name}})
+        echo ${VOLUME_BACKUP} >> ~/borgbackup-dev/backup-dirs.txt
     done
 
-    for volume in $(docker volume ls --filter "name=exch" --format={{.Name}}); do
-        VOLUME_BACKUP=$(docker volume ls --filter "name=${volume}" --format={{.Name}})
-        echo ${VOLUME_BACKUP} >> backup-dirs.txt
+    for volume in $(sudo docker volume ls --filter "name=exch" --format={{.Name}}); do
+        VOLUME_BACKUP=$(sudo docker volume ls --filter "name=${volume}" --format={{.Name}})
+        echo ${VOLUME_BACKUP} >> ~/borgbackup-dev/backup-dirs.txt
     done
 
-    for volume in $(docker volume ls --filter "name=data" --format={{.Name}}); do
-        VOLUME_BACKUP=$(docker volume ls --filter "name=${volume}" --format={{.Name}})
-        echo ${VOLUME_BACKUP} >> backup-dirs.txt
+    for volume in $(sudo docker volume ls --filter "name=data" --format={{.Name}}); do
+        VOLUME_BACKUP=$(sudo docker volume ls --filter "name=${volume}" --format={{.Name}})
+        echo ${VOLUME_BACKUP} >> ~/borgbackup-dev/backup-dirs.txt
     done
 
-    for volume in $(docker volume ls --filter "name=monitoring" --format={{.Name}}); do
-        VOLUME_BACKUP=$(docker volume ls --filter "name=${volume}" --format={{.Name}})
-        echo ${VOLUME_BACKUP} >> backup-dirs.txt
+    for volume in $(sudo docker volume ls --filter "name=monitoring" --format={{.Name}}); do
+        VOLUME_BACKUP=$(sudo docker volume ls --filter "name=${volume}" --format={{.Name}})
+        echo ${VOLUME_BACKUP} >> ~/borgbackup-dev/backup-dirs.txt
     done
-    #echo "Hostname contains 'docker'."
+   #echo "Hostname contains 'docker'."
     #logs_directory="/var/lib/docker/volumes"
     #find "$logs_directory" -type d -name "*logs*" -exec find {} -type f \; | while read -r log_file; do
         #log_filename=$(basename "$log_file")
         #scp -i "$SSH_KEY" "$log_file" "$BACKUP_USER@$BACKUP_SERVER:$BORG_REPO/$current_hostname-volumes-backup"
+        #echo "$log_file" >> backup-dirs.txt
     #done
 
     #find "$logs_directory" -type d -name "*exch*" -exec find {} -type f \; | while read -r log_file; do
@@ -82,8 +81,9 @@ if [[ $current_hostname == *"docker"* ]]; then
         #log_filename=$(basename "$log_file")
         #scp -i "$SSH_KEY" "$log_file" "$BACKUP_USER@$BACKUP_SERVER:$BORG_REPO/$current_hostname-volumes-backup"
     #done
-#fi
+fi
 
+#for dir in "${SOURCE_DIRS}"
 for dir in "$(cat ~/borgbackup-dev/backup-dirs.txt)"; do
     echo "Backing up $dir..."
     borg create -s --progress $BACKUP_USER@$BACKUP_SERVER:$BORG_REPO::$ARCHIVE_NAME $dir
@@ -97,7 +97,3 @@ done
 
 # Prune old backups on the local repository
 borg prune -v $BACKUP_USER@$BACKUP_SERVER:$BORG_REPO --keep-daily=7 --keep-weekly=4 --keep-monthly=1
-
-
-
-
